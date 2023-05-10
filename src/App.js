b7ec698b5e2c1bc39720cf79185322f42f56d729
APPLE <apple@OluwatiisiPhone.localdomain>
2023-05-10 13:25:02 +0100

    About page and link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 // you can create React components using classes or hooks
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
+import About from "./pages/About";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
   };
   const deleteFeedback = (id) => {
     if (window.confirm("Sure you wanna delete?")) {
@@ -20,16 +23,31 @@ function App() {
     }
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <Form handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Form handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
+      <Footer />
       {/* <h1>Twinwaters, Lagos</h1>
-      <p>The best leisure and dining experience</p> */}
-    </>
+    <p>The best leisure and dining experience</p> */}
+    </Router>
   );
 }
 
