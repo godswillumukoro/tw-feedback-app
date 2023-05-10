ad5fa129c4d8bb278b54ee7a76c746f7fa765607
APPLE <apple@OluwatiisiPhone.localdomain>
2023-05-10 13:33:44 +0100

    Reduce footer link size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className="about-link">
       <Link to="/about">
-        <FaQuestion size={30} />
+        <FaQuestion size={12} />
       </Link>
     </div>
   );
